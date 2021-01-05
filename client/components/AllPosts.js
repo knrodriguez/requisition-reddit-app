0885f55b001af3a50552991b818a309c3d754bac
Kirstie Rodriguez <kirstienrodriguez@gmail.com>
2021-01-05 13:44:54 -0500

    connected AllPosts to posts reducer

--- client/components/AllPosts.js
+++ client/components/AllPosts.js
@@ -1,18 +1,40 @@
+import { connect } from 'react-redux';
 import React from 'react';
 import RequisitionForm from './RequisitionForm';
 import Post from './Post';
+import { getPostsFromReddit } from '../reducers/postsReducer';
 
 class AllRequisitions extends React.Component {
+    constructor(){
+        super();
+        this.state({});
+    }
+
+    componentDidMount(){
+        props.getPostsFromReddit();
+    }
+
+
     render(){
         const posts = [];
         return (
             <div>
                 <RequisitionForm />
                 <div>
+                    <h2>Posts</h2>
                     {posts.map(post => <Post post={post} />)}
                 </div>
             </div>
         );
     }
 }
-export default AllRequisitions;
+
+const mapState = (state) => ({
+    posts: state.posts
+});
+
+const mapDispatch = (dispatch) => ({
+    getPostsFromReddit: () => dispatch(getPostsFromReddit())
+});
+
+export default connect(mapState, mapDispatch)(AllRequisitions);
