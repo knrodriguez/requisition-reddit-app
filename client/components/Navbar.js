45d2e99bcb0ed431685a8d27f4ead902e61748e1
Kirstie Rodriguez <kirstienrodriguez@gmail.com>
2021-01-07 18:15:07 -0500

    refactor: added posts menu item to navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import Switch from '@material-ui/core/Switch';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormGroup from '@material-ui/core/FormGroup';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import Button from '@material-ui/core/Button';
@@ -43,14 +40,20 @@ function Navbar(props) {
     };
 
     const handleClose = (action) => {
-        setAnchorEl(null);
         switch(action){
             case 'logout':
                 props.logout();
+                break;
             default:
-                return;
+                break;
         }
+        setAnchorEl(null);
     };
+
+    const goToPosts = () => {
+        setAnchorEl(null);
+        <Redirect to='/posts' />
+    }
 
     return (
         <div className={classes.root}>
@@ -85,8 +88,8 @@ function Navbar(props) {
                     open={open}
                     onClose={handleClose}
                 >
-                    <MenuItem onClick={handleClose}>Profile</MenuItem>
-                    <MenuItem onClick={handleClose}>My account</MenuItem>
+                    <MenuItem component={Link} to='/posts' onClick={handleClose}>Posts</MenuItem>
+                    <MenuItem onClick={handleClose}>My Profile</MenuItem>
                     <MenuItem onClick={() => handleClose('logout')}>Logout</MenuItem>
                 </Menu>
                 </div>
--- client/components/NotFound.js
+++ client/components/NotFound.js
@@ -3,8 +3,8 @@ import React from 'react';
 export const NotFound = (props) => {
     return (
         <div className='notFound'>
-            <h1>404</h1> <h2>Page Not Found</h2>
-            <h5>Here, have a calming image.</h5>
+            <h1>404 - Page Not Found</h1>
+            <h3>Here, have a calming image.</h3>
             <img src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.highreshdwallpapers.com%2Fwp-content%2Fuploads%2F2014%2F03%2FTranquil-Open-Paradise-1920x1080.jpg&f=1&nofb=1' />
         </div>
     )
