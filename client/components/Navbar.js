058d9ec80f021ed82e84aa8cea59c441de62830f
Kirstie Rodriguez <kirstienrodriguez@gmail.com>
2021-01-07 17:51:19 -0500

    cleanup

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -27,81 +27,6 @@ const useStyles = makeStyles((theme) => ({
     },
   }));
 
-// class Navbar extends React.Component {
-//     constructor(){
-//         super();
-//         this.state = {
-//             anchorEl: null,
-//             open: false,
-//         };
-//         this.handleClose = this.handleClose.bind(this);
-//         this.handleMenu = this.handleMenu.bind(this);
-//     }
-//     handleClose(){
-//         this.setState({anchorEl: null, open: false});
-//     }
-
-//     handleMenu(event){
-//         this.setState({ anchorEl: event.currentTarget, open: true })
-//     }
-
-//     render(){
-//         const classes = useStyles();
-//         return (
-//             <div>
-//                 <AppBar position="static">
-//                     <Toolbar>
-//                     <IconButton edge="start"  color="inherit" aria-label="menu">
-//                         <MenuIcon />
-//                     </IconButton>
-//                     <Typography variant="h6">
-//                         Requisition Reddit
-//                     </Typography>
-//                     {this.props.user && (
-//                         <div>
-//                         <IconButton
-//                             aria-label="account of current user"
-//                             aria-controls="menu-appbar"
-//                             aria-haspopup="true"
-//                             onClick={handleMenu}
-//                             color="inherit"
-//                         >
-//                             <AccountCircle />
-//                         </IconButton>
-//                         <Menu
-//                             id="menu-appbar"
-//                             anchorEl={anchorEl}
-//                             anchorOrigin={{
-//                             vertical: 'top',
-//                             horizontal: 'right',
-//                             }}
-//                             keepMounted
-//                             transformOrigin={{
-//                             vertical: 'top',
-//                             horizontal: 'right',
-//                             }}
-//                             open={open}
-//                             onClose={handleClose}
-//                         >
-//                             <MenuItem onClick={handleClose}>Profile</MenuItem>
-//                             <MenuItem onClick={handleClose}>My account</MenuItem>
-//                             <MenuItem onClick={handleClose}>Logout</MenuItem>
-//                         </Menu>
-//                         </div> )}
-//                     </Toolbar>
-//                 </AppBar>
-//             </div>
-//         )
-//     }
-// }
-
-// const mapState = (state) => ({
-//     user: state.user
-// })
-
-// export default connect(mapState, null)(Navbar); 
-
-
 function Navbar(props) {
     const classes = useStyles();
     const [auth, setAuth] = React.useState(true);
@@ -129,12 +54,6 @@ function Navbar(props) {
 
     return (
         <div className={classes.root}>
-        {/* <FormGroup>
-            <FormControlLabel
-            control={<Switch checked={auth} onChange={handleChange} aria-label="login switch" />}
-            label={auth ? 'Logout' : 'Login'}
-            />
-        </FormGroup> */}
         <AppBar position="static">
             <Toolbar>
             <Typography variant="h6" className={classes.title}>
