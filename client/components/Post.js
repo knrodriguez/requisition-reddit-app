9dcd5bee04b5adff90404a9bc217b45d6763570a
Kirstie Rodriguez <kirstienrodriguez@gmail.com>
2021-01-07 22:52:02 -0500

    feat: user can delete posts

--- client/components/Post.js
+++ client/components/Post.js
@@ -1,9 +1,10 @@
-import { CardActionArea, Typography, Card, CardActions, CardMedia, CardContent, Button, MenuItem } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 import React from 'react'
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { deletePost } from '../reducers/postsReducer';
+import { CardActionArea, Typography, Card, CardActions, CardMedia, 
+    CardContent, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
     root:{
